56a5deeefe3816c5be9a0fe8cf3b144b4f9d9bd1
Kimio Taniguchi <ghostnotesdot@gmail.com>
2018-10-21 00:03:00 +0900

    Adds Collapsing Toolbar. The font family of the title on the Toolbar is changed programmatically.

--- app/src/main/java/co/ghostnotes/sample/font/MainActivity.kt
+++ app/src/main/java/co/ghostnotes/sample/font/MainActivity.kt
@@ -2,17 +2,55 @@ package co.ghostnotes.sample.font
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.content.res.ResourcesCompat
+import android.view.Menu
+import android.view.MenuItem
 import co.ghostnotes.sample.font.ui.main.MainFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeLayout()
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun initializeLayout() {
+        setSupportActionBar(toolbar)
+
+        val typeface = ResourcesCompat.getFont(this, R.font.montserrat)
+        toolbar_layout.setCollapsedTitleTypeface(typeface)
+        toolbar_layout.setExpandedTitleTypeface(typeface)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
 
