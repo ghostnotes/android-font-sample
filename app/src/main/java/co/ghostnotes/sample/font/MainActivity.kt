c7c1cb393f4e93197df396c2471b367168b0c16a
Kimio Taniguchi <ghostnotesdot@gmail.com>
2018-10-20 14:01:23 +0900

    First source codes commit.

---
+++ app/src/main/java/co/ghostnotes/sample/font/MainActivity.kt
@@ -0,0 +1,19 @@
+package co.ghostnotes.sample.font
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import co.ghostnotes.sample.font.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
